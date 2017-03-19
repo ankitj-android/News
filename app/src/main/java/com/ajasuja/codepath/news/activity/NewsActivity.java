71eca8fcac96f5160618fb0376195a4059f5fae4
Ankit Jasuja <AJasuja@walmartlabs.com>
2017-03-19 01:52:53 -0700

    added infinite scroll for grid view

--- app/src/main/java/com/ajasuja/codepath/news/activity/NewsActivity.java
+++ app/src/main/java/com/ajasuja/codepath/news/activity/NewsActivity.java
@@ -3,6 +3,7 @@ package com.ajasuja.codepath.news.activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.MenuItemCompat;
@@ -21,6 +22,7 @@ import android.widget.GridView;
 import com.ajasuja.codepath.news.R;
 import com.ajasuja.codepath.news.adapter.NewsArticleAdapter;
 import com.ajasuja.codepath.news.fragment.SettingsDialogFragment;
+import com.ajasuja.codepath.news.listener.EndlessScrollListener;
 import com.ajasuja.codepath.news.model.NewsArticle;
 import com.ajasuja.codepath.news.model.Settings;
 import com.ajasuja.codepath.news.util.NetworkUtil;
@@ -57,11 +59,13 @@ public class NewsActivity extends AppCompatActivity implements SettingsDialogFra
     private View parentView;
 
 
+    final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
         parentView = findViewById(R.id.activity_news);
         connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         if (NetworkUtil.canCallService(connectivityManager)) {
@@ -79,7 +83,14 @@ public class NewsActivity extends AppCompatActivity implements SettingsDialogFra
         if (query == null) {
             query = formatQuery("donald trump");
         }
-        fetchArticles();
+        fetchArticles(0);
+        gridViewNews.setOnScrollListener(new EndlessScrollListener() {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                fetchArticles(page);
+                return true;
+            }
+        });
 //        gridViewNews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
 //            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -114,7 +125,7 @@ public class NewsActivity extends AppCompatActivity implements SettingsDialogFra
             @Override
             public boolean onQueryTextSubmit(String queryString) {
                 query = formatQuery(queryString);
-                fetchArticles();
+                fetchArticles(0);
                 // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
                 // see https://code.google.com/p/android/issues/detail?id=24599
                 searchView.clearFocus();
@@ -155,26 +166,9 @@ public class NewsActivity extends AppCompatActivity implements SettingsDialogFra
         settingsDialogFragment.show(fragmentManager, "Settings");
     }
 
-    private void fetchArticles() {
-        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
-//        String NY_TIMES_URL = String.format("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=%s&page=0&api-key=2ff5b653ccc749ed9bd0640dcec277da", query);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(settings.getBeginDateInMillis());
-        String year = String.valueOf(calendar.get(Calendar.YEAR));
-        int monthInteger = calendar.get(Calendar.MONTH) + 1;
-        String month = (monthInteger > 9) ? String.valueOf(monthInteger) : String.valueOf("0" + monthInteger);
-        String day = (calendar.get(Calendar.DAY_OF_MONTH) >= 10) ? String.valueOf(calendar.get(Calendar.DAY_OF_MONTH)) : String.valueOf("0" + calendar.get(Calendar.DAY_OF_MONTH));
-
-        String beginDate = year+month+day;//"20170112";
-        String sortOrder = settings.getSortOrder().getSortOrder().toLowerCase();
-        String newsDeskValues = toNewsDeskValues(settings);
-//        String newsDeskValues = "\"Sports\" \"Foreign\"";
-//        String beginDate = "20170112";
-//        String sortOrder = "oldest";
-//        String newsDeskValues = "\"Sports\" \"Foreign\"";
-        final String nyTimesUrl = buildNyTimesUrl(settings);
-//        String NY_TIMES_URL = String.format("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=%s&begin_date=%s&sort=%s&fq=news_desk:(%s)&api-key=2ff5b653ccc749ed9bd0640dcec277da",
-//                query.replace(' ', '+'), beginDate, sortOrder, newsDeskValues);
+    private void fetchArticles(final int page) {
+        final AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        final String nyTimesUrl = buildNyTimesUrl(page, settings);
         Log.d("debug", nyTimesUrl);
         asyncHttpClient.get(nyTimesUrl, new JsonHttpResponseHandler(){
             @Override
@@ -182,7 +176,9 @@ public class NewsActivity extends AppCompatActivity implements SettingsDialogFra
                 super.onSuccess(statusCode, headers, response);
                 Log.d("[RESPONSE]", response.toString());
                 try {
-                    newsArticles.clear();
+                    if (page == 0) {
+                        newsArticles.clear();
+                    }
                     newsArticles.addAll(fromJsonArray(response.getJSONObject("response").getJSONArray("docs")));
                     newsArticleAdapter.notifyDataSetChanged();
                     Log.d("[DEBUG]", newsArticles.toString());
@@ -193,15 +189,23 @@ public class NewsActivity extends AppCompatActivity implements SettingsDialogFra
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                super.onFailure(statusCode, headers, throwable, errorResponse);
-                Snackbar.make(parentView, "Failed NyTimes Api Call : " + nyTimesUrl, Snackbar.LENGTH_SHORT).show();
-                Log.d("debug", throwable.toString());
+                String errorMessage = "Failed NyTimes Api Call : " + statusCode + nyTimesUrl;
+                Log.d("debug", errorMessage);
+                if (statusCode == 429) {
+                    try {
+                        Snackbar.make(parentView, "API Rate Limiting", Snackbar.LENGTH_SHORT).show();
+                        Thread.sleep(1000);
+                        fetchArticles(page);
+                    } catch (InterruptedException e) {
+                        super.onFailure(statusCode, headers, throwable, errorResponse);
+                    }
+                }
             }
         });
-
     }
 
-    private String buildNyTimesUrl(Settings settings) {
+
+    private String buildNyTimesUrl(int page, Settings settings) {
         StringBuilder urlBuilder = new StringBuilder();
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(settings.getBeginDateInMillis());
@@ -212,7 +216,9 @@ public class NewsActivity extends AppCompatActivity implements SettingsDialogFra
         String beginDate = year+month+day;//"20170112";
         String sortOrder = settings.getSortOrder().getSortOrder().toLowerCase();
         String newsDeskValues = toNewsDeskValues(settings);
-        String urlWithoutNewsDesk = String.format("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=%s&begin_date=%s&sort=%s&api-key=2ff5b653ccc749ed9bd0640dcec277da", query, beginDate, sortOrder);
+        String urlWithoutNewsDesk = String.format("https://api.nytimes.com/svc/search/v2/articlesearch.json?" +
+                "q=%s&page=%d&begin_date=%s&sort=%s&api-key=2ff5b653ccc749ed9bd0640dcec277da",
+                query, page, beginDate, sortOrder);
         urlBuilder.append(urlWithoutNewsDesk);
         if (newsDeskValues != null && !newsDeskValues.isEmpty()) {
             urlBuilder.append(String.format("&fq=news_desk:(%s)", toNewsDeskValues(settings)));
@@ -240,7 +246,7 @@ public class NewsActivity extends AppCompatActivity implements SettingsDialogFra
     @Override
     public void onSettingsSave(Settings settings) {
         this.settings = settings;
-        fetchArticles();
+        fetchArticles(0);
         Log.d("DEBUG", "Back to settings from SettingsDialogFragment with settings : " + settings);
     }
 }
